a4cac06c3e9c4a4ef14a7be2f4d367f00bdc2af2
Alex Pace <pacealexwork@gmail.com>
2025-03-05 17:48:40 +0100

    HomeScreen content moved to pages/ package

--- composeApp/src/commonMain/kotlin/dev/alexpace/kassist/navigation/screens/shared/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/alexpace/kassist/navigation/screens/shared/HomeScreen.kt
@@ -1,42 +1,16 @@
 package dev.alexpace.kassist.navigation.screens.shared
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import dev.alexpace.kassist.navigation.screens.supporter.SupporterScreen
-import dev.alexpace.kassist.navigation.screens.victim.VictimScreen
+import dev.alexpace.kassist.pages.HomePage
 
 class HomeScreen : Screen {
     @Composable
     override fun Content() {
         // Declaring non-nullable navigator
         val navigator = LocalNavigator.currentOrThrow
-
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column {
-                Text("Initial screen")
-
-                Button(
-                    onClick = { navigator.push(VictimScreen()) }
-                ) {
-                    Text("Go to victim screen")
-                }
-
-                Button(
-                    onClick = { navigator.push(SupporterScreen()) }
-                ) {
-                    Text("Go to supporter screen")
-                }
-            }
-        }
+        HomePage(navigator)
     }
 }
