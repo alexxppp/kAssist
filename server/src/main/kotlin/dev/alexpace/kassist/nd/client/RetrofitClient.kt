4a170cf73a81a7fb51a1c7d362476bb1b6793aa8
Alex Pace <pacealexwork@gmail.com>
2025-03-08 19:31:28 +0100

    Singleton Retrofit Client initialized

---
+++ server/src/main/kotlin/dev/alexpace/kassist/nd/client/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package dev.alexpace.kassist.nd.client
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+// Singleton
+object RetrofitClient {
+
+    private const val BASE_URL = "https://www.gdacs.org/gdacsapi/api/events/geteventlist/EVENTS4APP"
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    val retrofit: Retrofit by lazy {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
