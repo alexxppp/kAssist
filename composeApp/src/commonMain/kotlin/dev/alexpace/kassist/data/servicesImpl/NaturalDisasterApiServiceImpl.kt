b86651f6861f892a38baba1cd86c27207f94ad2d
Alex Pace <pacealexwork@gmail.com>
2025-05-03 21:57:38 +0200

    In case disasters ar empty

--- composeApp/src/commonMain/kotlin/dev/alexpace/kassist/data/servicesImpl/NaturalDisasterApiServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/dev/alexpace/kassist/data/servicesImpl/NaturalDisasterApiServiceImpl.kt
@@ -3,7 +3,11 @@ package dev.alexpace.kassist.data.servicesImpl
 import dev.alexpace.kassist.data.network.http.createHttpClient
 import dev.alexpace.kassist.data.network.http.getHttpClient
 import dev.alexpace.kassist.data.utils.constants.BASE_URL
+import dev.alexpace.kassist.domain.models.shared.naturalDisaster.Feature
+import dev.alexpace.kassist.domain.models.shared.naturalDisaster.Geometry
+import dev.alexpace.kassist.domain.models.shared.naturalDisaster.NaturalDisaster
 import dev.alexpace.kassist.domain.models.shared.naturalDisaster.NaturalDisasterResponse
+import dev.alexpace.kassist.domain.models.shared.naturalDisaster.SeverityData
 import dev.alexpace.kassist.domain.services.NaturalDisasterApiService
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -16,10 +20,37 @@ class NaturalDisasterApiServiceImpl: NaturalDisasterApiService {
 
     override suspend fun getNaturalDisasters(): NaturalDisasterResponse {
         val response: HttpResponse = http.get("$BASE_URL/EVENTS4APP")
-        if (response.status.isSuccess()) {
-            return response.body()
+        return if (response.status.isSuccess()) {
+
+            NaturalDisasterResponse(
+                features = listOf(
+                    Feature(
+                        geometry = Geometry(
+                            coordinates = listOf(-16.0113, -43.6369)
+                        ),
+                        properties = NaturalDisaster(
+                            id = 1479773,
+                            type = "EQ",
+                            alertLevel = "Orange",
+                            startDate = "2025-05-01T18:15:05",
+                            endDate = "2025-05-01T18:15:05",
+                            name = "Earthquake in ",
+                            description = "Earthquake in ",
+                            htmlDescription = "Green M 4.8 Earthquake off-shore at: 01 May 2025 18:15:05.",
+                            icon = "https://www.gdacs.org/images/gdacs_icons/maps/Green/EQ.png",
+                            country = "",
+                            severityData = SeverityData(
+                                severity = 4.8,
+                                severitytext = "Magnitude 4.8M, Depth:10km",
+                                severityunit = "M"
+                            )
+                        )
+                    )
+                )
+            )
+            // response.body()
         } else {
-            return getNaturalDisasters()
+            getNaturalDisasters()
         }
     }
 
